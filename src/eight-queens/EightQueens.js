8ffd27b9cb364d585faa6abd3b581caf38aa5188
shaack <shaack@gmail.com>
2023-07-04 23:16:23 +0200

    Added basic project structure

---
+++ src/eight-queens/EightQueens.js
@@ -0,0 +1,12 @@
+/**
+ * @author Stefan Haack (https://shaack.com), all rights reserved
+ */
+import {Chessboard} from "cm-chessboard/src/Chessboard.js"
+import {Extension} from "cm-chessboard/src/model/Extension.js"
+
+export class EightQueens extends Extension {
+    constructor(chessboard, props = {}) {
+        super(chessboard)
+
+    }
+}
