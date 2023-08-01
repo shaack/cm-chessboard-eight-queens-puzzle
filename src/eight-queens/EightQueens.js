2e2c70c92c00fab5588b69b00dc656f506c4250c
shaack <shaack@gmail.com>
2023-08-01 17:30:07 +0200

    better moves mechanics

--- src/eight-queens/EightQueens.js
+++ src/eight-queens/EightQueens.js
@@ -5,29 +5,45 @@ import {INPUT_EVENT_TYPE} from "cm-chessboard/src/Chessboard.js"
 import {Extension} from "cm-chessboard/src/model/Extension.js"
 import {MARKER_TYPE, Markers} from "cm-chessboard/src/extensions/markers/Markers.js"
 import {Position} from "cm-chessboard/src/model/Position.js"
+import {MOVE_CANCELED_REASON} from "cm-chessboard/src/view/VisualMoveInput.js"
 
 export class EightQueens extends Extension {
-    constructor(chessboard, props = {}) {
+    constructor(chessboard) {
         super(chessboard)
         chessboard.addExtension(Markers)
         chessboard.initialized.then(() => {
-            chessboard.enableSquareSelect((event) => {
-                console.log(event.mouseEvent.type)
-                if(event.mouseEvent.type === "mousedown") {
-                    if(this.chessboard.getPiece(event.square)) {
-                        this.chessboard.setPiece(event.square, null)
-                    } else {
-                        this.chessboard.setPiece(event.square, "bq")
-                    }
-                    this.markThreatened()
+            this.clickListener = this.onSquareClick.bind(this)
+            chessboard.context.addEventListener("click", this.clickListener)
+            chessboard.enableMoveInput((event) => {
+                switch (event.type) {
+                    case INPUT_EVENT_TYPE.moveInputStarted:
+                        return true
+                    case INPUT_EVENT_TYPE.moveInputCanceled:
+                        if (event.reason === MOVE_CANCELED_REASON.movedOutOfBoard) {
+                            this.chessboard.setPiece(event.squareFrom, null)
+                        }
+                        return true
+                    case INPUT_EVENT_TYPE.validateMoveInput:
+                        return true
                 }
+                this.markThreatened()
             })
             this.markThreatened()
         })
     }
 
+    onSquareClick(event) {
+        const square = event.target.getAttribute("data-square")
+        if (square && !this.chessboard.getPiece(square)) {
+            const pieces = this.chessboard.state.position.getPieces()
+            if(pieces.length < 8) {
+                this.chessboard.setPiece(square, "bq")
+                this.markThreatened()
+            }
+        }
+    }
+
     markThreatened() {
-        console.log(this.chessboard.state.position.squares)
         this.chessboard.removeMarkers()
         let i = 0
         for (let square of this.chessboard.state.position.squares) {
@@ -46,7 +62,7 @@ export class EightQueens extends Extension {
 
     isThreatened(square) {
         const [x, y] = Position.squareToCoordinates(square)
-        console.log("isThreatened", square, x, y)
+        // console.log("isThreatened", square, x, y)
         // test file
         for (let sx = 0; sx < 8; sx++) {
             const sSquare = Position.coordinatesToSquare([sx, y])
@@ -55,7 +71,7 @@ export class EightQueens extends Extension {
             }
             const piece = this.chessboard.getPiece(sSquare)
             if (piece) {
-                console.log("a) threatened by", piece, sSquare)
+                // console.log("a) threatened by", piece, sSquare)
                 return true
             }
         }
@@ -67,7 +83,7 @@ export class EightQueens extends Extension {
             }
             const piece = this.chessboard.getPiece(sSquare)
             if (piece) {
-                console.log("b) threatened by", piece, sSquare)
+                // console.log("b) threatened by", piece, sSquare)
                 return true
             }
         }
@@ -83,7 +99,7 @@ export class EightQueens extends Extension {
             }
             const piece = this.chessboard.getPiece(sSquare)
             if (piece) {
-                console.log("c) threatened by", piece, sSquare)
+                // console.log("c) threatened by", piece, sSquare)
                 return true
             }
         }
@@ -99,7 +115,7 @@ export class EightQueens extends Extension {
             }
             const piece = this.chessboard.getPiece(sSquare)
             if (piece) {
-                console.log("d) threatened by", piece, sSquare)
+                // console.log("d) threatened by", piece, sSquare)
                 return true
             }
         }
